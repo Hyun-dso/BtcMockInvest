f50c8ffd0ede0e6bb350bd09ef56d32a8cc4d717
Hyun-dso <dltkdgus0413@gmail.com>
2025-06-12 15:23:31 +0900

    백필 러너 설명 주석으로 추가
    백필 러너 설명 주석으로 추가

--- src/main/java/kim/donghyun/util/BackfillRunner.java
+++ src/main/java/kim/donghyun/util/BackfillRunner.java
@@ -2,21 +2,16 @@ package kim.donghyun.util;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import kim.donghyun.service.Backfill.BatchCandleAggregator;
 import kim.donghyun.service.Backfill.BinanceBackfillService;
 
+// 백필 러너 1분봉 데이터를 받아옴 (시간 설정은 BinanceBackfillService에서)
+// 이후 Backfill/AggregationRunner로 1분봉을 기반으로 나머지 봉 집계
 public class BackfillRunner {
     public static void main(String[] args) throws Exception {
         try (ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("backfill-context.xml")) {
             BinanceBackfillService service = ctx.getBean(BinanceBackfillService.class);
             service.backfill();
             
-            BatchCandleAggregator aggregator = ctx.getBean(BatchCandleAggregator.class);
-            aggregator.generate15MinCandle();
-            aggregator.generate1HourCandle();
-            aggregator.generate1DayCandle();
-            aggregator.generate1WeekCandle();
-            aggregator.generate1MonthCandle();
         }
     }
 }
